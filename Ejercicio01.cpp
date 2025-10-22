be44b5a2ab07d340b9e73e9197eeba7de3f7ac6f
Yordano Boza <ybozap@unsa.edu.pe>
2025-10-22 11:54:41 -0500

    División segura

---
+++ Ejercicio01.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double num1, num2;
+    
+    cout << "Ingrese el primer numero: ";
+    cin >> num1;
+    cout << "Ingrese el segundo numero: ";
+    cin >> num2;
+
+    try {
+        if (num2 == 0) {
+            throw runtime_error("Error: No se puede dividir por cero.");
+        }
+        double resultado = num1 / num2;
+        cout << "El resultado de la division es: " << resultado << endl;
+    }
+    catch (const runtime_error& e) {
+        cout << e.what() << endl;
+    }
+    
+    cout << "Programa finalizado. Gracias por usar el sistema." << endl;
+
+    return 0;
+}
